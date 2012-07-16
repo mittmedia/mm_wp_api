27aaa72dd99292a274ebfe6058ed014110b31324
Fredrik Sundström <fredrik.sundstrom@gmail.com>
2012-07-16 12:28:57 +0200

    Added text-domain

--- views/mm_wp_api/index.html.php
+++ views/mm_wp_api/index.html.php
@@ -2,6 +2,6 @@
 
 <div class="wrap mm_wp_api">
   <div id="icon-options-general" class="icon32"><br></div>
-  <h2><?php _e( 'Mittmedia Wordpress API' ); ?></h2>
+  <h2><?php _e( 'Mittmedia Wordpress API', 'mm-wp-api') ); ?></h2>
   Nothing here.
 </div>
